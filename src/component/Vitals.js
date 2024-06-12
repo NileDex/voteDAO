6038e038501fe3a76026c5eb7372e572cd282727
Andy Golay <andygolay@gmail.com>
2024-06-12 08:55:38 -0400

    A couple cosmetic fixes to homepage
    Simple example pull request (PR)

--- src/component/Vitals.js
+++ src/component/Vitals.js
@@ -3,8 +3,8 @@ const vitals = () => {
     return ( 
         <div className="container">
             <h1>MOVE DAO</h1>
-            <h4>The First Voting Platform for Movement Blockchain M1</h4>
-            <div className="fol-cont"><a href="https://x.com/movementlabsxyz" className="twitterico"><BsTwitterX /></a>Follow @Movementlabs</div>
+            <h4>The First Voting Platform for Movement Network</h4>
+            <div className="fol-cont"><a href="https://x.com/movementlabsxyz" className="twitterico"><BsTwitterX /></a>Follow @movementlabsxyz</div>
         </div>
 
      );
