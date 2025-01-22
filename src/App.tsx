6bb88e209051f6d0e50a0b5d49e884c2400bcdd6
NileDex <152003924+NileDex@users.noreply.github.com>
2025-01-22 08:40:54 +0100

    Update changes to pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-=======
-// import { useState } from "react";
-// import { MdCancel } from "react-icons/md";
-// import "./App.css";
-// import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-// import Socials from "./components/Socials";
-// import Header from "./components/Header";
-// import Vitals from "./components/Vitals";
-// import VoteData from "./components/VoteData";
-// import Proposals from "./components/Proposals";
-// import FAQ from "./components/FAQ";
-// import Move from "./components/votecomponent/Vote";
-
-// function App() {
-//   const [showHeader, setShowHeader] = useState(true);
-
-//   const handleCancelClick = () => {
-//     setShowHeader(false);
-//   };
-
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-
-//         <Route
-//           path="/"
-//           element={
-//             <div className="App">
-//               <div>
-//               <div className="hero-background"></div>
-//                 {showHeader && (
-//                   <div className="socials">
-//                     <Socials />
-//                     <p className="cancel-button" onClick={handleCancelClick}>
-//                       <MdCancel />
-//                     </p>
-//                   </div>
-//                 )}
-//               </div>
-//               <Header />
-//               <Vitals />
-//               <VoteData />
-//               <Proposals />
-//               <FAQ />
-
-//             </div>
-//           }
-//         />
-//         <Route path="/vote" element={<Move />} />
-//         <Route path="/vote2" element={<Move />} />
-//         <Route path="/vote3" element={<Move />} />
-//         <Route path="*" element={<Navigate to="/" />} />{" "}
-//         {/* Redirect unknown paths to landing page */}
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
->>>>>>> 4cf47d670c645f87e68663261e134d4b5448458c
 import { useState } from "react";
 import { MdCancel } from "react-icons/md";
 import "./App.css";
@@ -110,13 +49,8 @@ function App() {
         />
         <Route path="/profile" element={<Profile />} />
         <Route path="/analytics" element={<AnalyticsPage />} />
-<<<<<<< HEAD
-        <Route path="/CommunityGovernance" element={ <SimpleCards/> } />
-        <Route path="/vote" element={<Move />} />
-=======
         {/* <Route path="/CommunityGovernance" element={<CommunityGovernance/>} /> */}
         <Route path="/vote" element={<Vote />} />
->>>>>>> 4cf47d670c645f87e68663261e134d4b5448458c
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
